66d7585af80efd4a9676a60caf40cd338f305c4b
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-04-07 01:51:38 +0200

    .

---
+++ mealsApp/meals_app/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../screens/categories_screen.dart';
+import '../screens/category_meals_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.ś
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: Colors.amber,
+          primarySwatch: Colors.pink,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+                fontFamily: 'Raleway',
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+                fontFamily: 'Raleway',
+              ),
+              headline5: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+              headline6: const TextStyle(
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+      ),
+      routes: {
+        '/': (context) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
+      },
+    );
+  }
+}
---
+++ mealsApp/meals_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+  const CategoryMealsScreen({
+    Key? key,
+    /*required this.categoryId, required this.categoryTitle*/
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          var item = categoryMeals[index];
+          return MealItem(
+            title: item.title,
+            imageUrl: item.imageUrl,
+            duration: item.duration,
+            complexity: item.complexity,
+            affordability: item.affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
+
+class CategoryMealsScreenArguments {
+  final String categoryTitle;
+  final String message;
+
+  CategoryMealsScreenArguments(this.categoryTitle, this.message);
+}
