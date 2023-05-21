f331b30ce11ce7bedfbc56a01c8fd828b67f3b81
Daniel Józefiuk <daniel.jozefiuk@gmail.com>
2023-05-21 16:02:19 +0200

    Some fun

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors})
+  
+  const GradientContainer.purple({super.key})
+    : colors = const [Colors.deepPurple, Colors.deepPurpleAccent];
+
+  final List<Color> colors;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+        ),
+      ),
+      child: const Center(
+        child: Image.asset('name')
+      ),
+    );
+  }
+}
