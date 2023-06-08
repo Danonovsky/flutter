2376a94ab836cd236170a78ae2eee81bace3ed0a
Danonovsky <daniel.jozefiuk@gmail.com>
2023-06-09 01:36:12 +0200

    Wow so much progress

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
@@ -17,29 +18,8 @@ class GradientContainer extends StatelessWidget {
           begin: Alignment.topLeft,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-1.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
